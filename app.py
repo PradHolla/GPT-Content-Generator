3de27c37b8e7ffff7624dfc488b125e490c903db
Pradhyumna N Holla <pradholla@gmail.com>
2023-04-24 18:56:26 +0530

    Changes

--- app.py
+++ app.py
@@ -14,23 +14,21 @@ st.sidebar.title("Upload your text file")
 file = st.sidebar.file_uploader("Upload your text file", type=["txt"])
 
 if file is not None:
-    file_details = {"FileName":file.name,"FileType":file.type,"FileSize":file.size}
-    # st.write(file_details)
-
     text = file.read().decode("utf-8")
-    # st.write(text)
+
     st.write("### Give instructions to the model:")
     prompt = st.text_area("_", label_visibility="hidden", height=200)
 
     if st.button("Generate"):
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[{
-            "role": "system", "content": f"{prompt}. \n\nText file: {text}. \n\nAnswer:"
-            }],
-            temperature=1,
-            # top_p=0,
-            # frequency_penalty=0,
-            # presence_penalty=0,
-        )
-        st.write(response["choices"][0]["message"]["content"])
+        with st.spinner("Generating..."):
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=[{
+                "role": "system", "content": f"{prompt}. \n\nText file: {text}. \n\nAnswer:"
+                }],
+                temperature=1,
+                # top_p=0,
+                # frequency_penalty=0,
+                # presence_penalty=0,
+            )
+            st.write(response["choices"][0]["message"]["content"])
