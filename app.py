c6aab33f3640bddd303fbd2f58b7ac1a6bee9e75
Pradhyumna N Holla <pradholla@gmail.com>
2023-05-11 17:50:21 +0530

    something

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ df1 = pd.read_excel('Lyzr Reco - Sales DB.xlsx', sheet_name='Fields to Map')
 df2 = pd.read_excel('Lyzr Reco - Sales DB.xlsx', sheet_name='Lyzr Reco Database')
 df3 = pd.read_excel('Lyzr Reco - Sales DB.xlsx', sheet_name='Draft')
 dataset = pd.read_csv('Superstore.csv')
-# if file is not None:
-#     text = pd.read_csv(file)
+
 st.write("### Give instructions to the model:")
 prompt = st.text_area("_", label_visibility="hidden", height=200)
 
